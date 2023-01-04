355e15e53e477a6f90565e99d4056bef5c347fc5
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-05 01:28:35 +0300

    Defining our STK push method

--- Mpesa_SDK/app/controllers/mpesas_controller.rb
+++ Mpesa_SDK/app/controllers/mpesas_controller.rb
@@ -4,7 +4,8 @@ class MpesasController < ApplicationController
 
     #Generate and get Access token from the Authorized API.
     private
-
+    
+    #Generate 
     def generate_access_token_request
         @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
         @consumer_key = ENV['MPESA_CONSUMER_KEY']
@@ -19,6 +20,7 @@ class MpesasController < ApplicationController
         res
     end
 
+    #Access token 
     def get_access_token
         res = generate_access_token_request()
         if res.code != 200
@@ -32,6 +34,53 @@ class MpesasController < ApplicationController
         AccessToken.destroy_all()
         AccessToken.create!(token: token)
         token
+    end
+
+        #Defined our STK Push
+    def stkpush
+        phoneNumber = params[:phoneNumber]
+        amount = params[:amount]
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
+        business_short_code = ENV["MPESA_SHORTCODE"]
+        password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
+        payload = {
+        'BusinessShortCode': business_short_code,
+        'Password': password,
+        'Timestamp': timestamp,
+        'TransactionType': "CustomerPayBillOnline",
+        'Amount': amount,
+        'PartyA': phoneNumber,
+        'PartyB': business_short_code,
+        'PhoneNumber': phoneNumber,
+        'CallBackURL': "#{ENV["CALLBACK_URL"]}/callback_url",
+        'AccountReference': 'Codearn',
+        'TransactionDesc': "Payment for Codearn premium"
+        }.to_json
+
+        headers = {
+        Content_type: 'application/json',
+        Authorization: "Bearer #{get_access_token}"
+        }
+
+        response = RestClient::Request.new({
+        method: :post,
+        url: url,
+        payload: payload,
+        headers: headers
+        }).execute do |response, request|
+        case response.code
+        when 500
+        [ :error, JSON.parse(response.to_str) ]
+        when 400
+        [ :error, JSON.parse(response.to_str) ]
+        when 200
+        [ :success, JSON.parse(response.to_str) ]
+        else
+        fail "Invalid response #{response.to_str} received."
+        end
+        end
+        render json: response
     end
  
 end
