29c1f17de653b603608b13808e309a7b46f9ef44
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-05 01:18:44 +0300

    Generate and get Access token from Authorized API

--- Mpesa_SDK/app/controllers/mpesas_controller.rb
+++ Mpesa_SDK/app/controllers/mpesas_controller.rb
@@ -1,2 +1,37 @@
+require 'rest-client'
+
 class MpesasController < ApplicationController
+
+    #Generate and get Access token from the Authorized API.
+    private
+
+    def generate_access_token_request
+        @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+        @consumer_key = ENV['MPESA_CONSUMER_KEY']
+        @consumer_secret = ENV['MPESA_CONSUMER_SECRET']
+        @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}")
+        headers = {
+            Authorization: "Bearer #{@userpass}"
+        }
+        res = RestClient::Request.execute( url: @url, method: :get, headers: {
+            Authorization: "Basic #{@userpass}"
+        })
+        res
+    end
+
+    def get_access_token
+        res = generate_access_token_request()
+        if res.code != 200
+        r = generate_access_token_request()
+        if res.code != 200
+        raise MpesaError('Unable to generate access token')
+        end
+        end
+        body = JSON.parse(res, { symbolize_names: true })
+        token = body[:access_token]
+        AccessToken.destroy_all()
+        AccessToken.create!(token: token)
+        token
+    end
+ 
 end
