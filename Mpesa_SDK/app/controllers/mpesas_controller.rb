f6db02a148ad99ecaffc3986736ab1fa5cc7acd8
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2023-01-05 09:14:38 +0300

    STK Query Request

--- Mpesa_SDK/app/controllers/mpesas_controller.rb
+++ Mpesa_SDK/app/controllers/mpesas_controller.rb
@@ -1,7 +1,5 @@
 class MpesasController < ApplicationController
-    require 'rest-client'
-
-
+        require 'rest-client'
         #Defined our STK Push
         def stkpush
             phoneNumber = params[:phoneNumber]
@@ -48,6 +46,47 @@ class MpesasController < ApplicationController
             end
             render json: response
         end
+
+        #Stk query Request
+        
+        def stkquery
+            url = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+            timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}"
+            business_short_code = ENV["MPESA_SHORTCODE"]
+            password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}")
+            payload = {
+            'BusinessShortCode': business_short_code,
+            'Password': password,
+            'Timestamp': timestamp,
+            'CheckoutRequestID': params[:checkoutRequestID]
+            }.to_json
+    
+            headers = {
+            Content_type: 'application/json',
+            Authorization: "Bearer #{ get_access_token }"
+            }
+    
+            response = RestClient::Request.new({
+            method: :post,
+            url: url,
+            payload: payload,
+            headers: headers
+            }).execute do |response, request|
+            case response.code
+            when 500
+            [ :error, JSON.parse(response.to_str) ]
+            when 400
+            [ :error, JSON.parse(response.to_str) ]
+            when 200
+            [ :success, JSON.parse(response.to_str) ]
+            else
+            fail "Invalid response #{response.to_str} received."
+            end
+            end
+            render json: response
+        end
+    
+
 
     #Generate and get Access token from the Authorized API.
     private
